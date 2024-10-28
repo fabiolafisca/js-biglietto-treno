fca78ff2efc5c9c9ccd8e275a50bb0468e0ac324
fabiolafisca <fabiolafisca@hotmail.com>
2024-10-28 13:20:50 +0100

    esercizio-biglietto-treno

---
+++ script/script.js
@@ -0,0 +1,22 @@
+/*
+
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+- L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+*/
+
+/* Prezzo del biglietto */
+// Chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
+const chilometri = parseFloat(prompt("Inserisci il numero di chilometri che vuoi percorrere:"));
+const etàPasseggero = parseInt(prompt("Inserisci l'età del passeggero:"));
+
+
+// Il prezzo del biglietto in base ai km 
+
+
+
+
